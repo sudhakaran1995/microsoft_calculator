ca2912ea0945bcd6ce191e11d20de9027c0299f4
sudhakaran1995 <71376083+sudhakaran1995@users.noreply.github.com>
2020-10-07 19:52:05 +0530

    updated multiuse addition

--- addition.py
+++ addition.py
@@ -1,10 +1,14 @@
 x=5
 y=4
+z=5
 
 
-def add(x,y):
-    return x+y
+def add(*args):
+    sum=0
+    for i in args:
+        sum+=i
+    return sum
 
-a=add(x,y)
+a=add(x,y,z)
 print(a)
 print('this is an addition function')
