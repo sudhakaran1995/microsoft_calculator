a806a6e86e4be41efb2061db91a575d967ba0adf
sudhakaran <sudhakarvijay41@gmail.com>
2020-10-07 20:07:07 +0530

    only for lchandra

--- addition.py
+++ addition.py
@@ -12,3 +12,16 @@ def add(*args):
 a=add(x,y,z)
 print(a)
 print('this is an addition function')
+
+
+# this is only for branch lchandra
+
+def sub(*args):
+    sum=0
+    for i in args:
+        sum-=i
+    return sum
+
+
+s=sub(x,y,z)
+print('subraction ans',s)
